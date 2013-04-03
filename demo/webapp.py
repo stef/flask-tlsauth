a6a8368a73448a0669e16ae1cd4986ca74ec7ece
stef <stefan.marsiske@gmail.com>
2013-04-03 17:43:28 +0200

    [enh] adopted to tlsauth 0.3

--- demo/webapp.py
+++ demo/webapp.py
@@ -2,24 +2,21 @@
 
 # run with
 # env/bin/uwsgi --socket 127.0.0.1:8080 --chdir $PWD/demo -pp $PWD -w tlsauth_wsgi -p 1 --virtualenv $PWD/env --py-autoreload 1
+# also create a ca in ../../x509-ca - for more info see tlsauth README
 
 from flask import Flask, Response
 import os
-Flask.secret_key = 'zxcvzxcvz'
 app = Flask(__name__)
+app.secret_key = 'zxcvzxcvz'
+#app.debug = True
 
 from tlsauth import CertAuthority
 import flask_tlsauth as tlsauth
 
-ca=CertAuthority('/home/stef/tasks/tlsauth/CA/public/root.pem',
-                 '/home/stef/tasks/tlsauth/CA/private/root.pem',
-                 '/home/stef/tasks/tlsauth/CA/conf/serial',
-                 '/home/stef/tasks/tlsauth/CA/dummy.pem',
-                 'http://www.example.com/crl.pem',
-                 '/home/stef/tasks/tlsauth/CA/incoming',
-                 )
+ca=CertAuthority('../../x509-ca')
 
-adminOs=['ctrlc','ctrlCA']
+app.debug = True
+adminOs=['CA admins']
 tlsauth.tlsauth_init(app, ca, groups=adminOs)
 
 @app.route('/hello')
